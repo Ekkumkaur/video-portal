44930af650856dc6342841502b7fc1be3af99b82
Ekkumkaur <bhardwajekta2000@gmail.com>
2025-12-19 18:30:09 +0530

    feat: add Axios API helper with base URL, auth token interceptor, and 401 error handling.

--- frontend/src/apihelper/api.ts
+++ frontend/src/apihelper/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+    // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
     // baseURL: 'http://72.61.239.4:5000',
-    // baseURL: 'https://brpl.net/api',
+    baseURL: 'https://brpl.net/api',
     headers: {
         'Content-Type': 'application/json',
     },
